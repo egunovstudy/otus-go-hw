d0e04cd733ea56ce133741cb76ad247429446af1
gleb_egunov <egunov.g.a@gmail.com>
2026-01-02 22:32:18 +0300

    HW3 - implemented str frequency counter

--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -44,7 +44,7 @@ var text = `Как видите, он  спускается  по  лестни�
 		В этот вечер...`
 
 func TestTop10(t *testing.T) {
-	t.Run("no words in empty string", func(t *testing.T) {
+	t.Run("no words in empty name", func(t *testing.T) {
 		require.Len(t, Top10(""), 0)
 	})
 
