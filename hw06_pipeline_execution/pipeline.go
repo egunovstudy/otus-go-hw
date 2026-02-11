08da94129b9c069eca6d3c62ce98f37a97f87367
gleb_egunov <egunov.g.a@gmail.com>
2026-02-11 07:57:10 +0300

    hw06_pipeline_execution - implemented pipeline execution, tests were added

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,72 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+
+	stream := wrapWithDone(in, done)
+	for _, stage := range stages {
+		stream = wrapWithDone(stage(stream), done)
+	}
+	return stream
+}
+
+func wrapWithDone(in In, done In) Out {
+	if in == nil {
+		return nil
+	}
+
+	out := make(Bi)
+
+	if done == nil {
+		go func() {
+			defer close(out)
+			for v := range in {
+				out <- v
+			}
+		}()
+		return out
+	}
+
+	go func() {
+		closed := false
+		defer func() {
+			if !closed {
+				close(out)
+			}
+		}()
+
+		for {
+			select {
+			case <-done:
+				if !closed {
+					close(out)
+					closed = true
+				}
+
+				for range in {
+					// выкидываем
+				}
+				return
+
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+				if closed {
+					continue
+				}
+
+				select {
+				case <-done:
+					close(out)
+					closed = true
+					for range in {
+					}
+					return
+				case out <- v:
+				}
+			}
+		}
+	}()
+
+	return out
 }
